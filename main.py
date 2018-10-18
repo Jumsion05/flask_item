9b9bf8c1c17e49ec35c92d2a725939eb9fdf6a6f
Jumsion05 <871888414@qq.com>
2018-10-18 18:35:29 +0800

    add sqlalchemy config info

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+class Config(object):
+    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:mysql@127.0.0.1:3306/python06"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_ECHO"] = True
+
+app.config.from_object(Config)
+
 @app.route("/")
 def index():
     return "index page"
