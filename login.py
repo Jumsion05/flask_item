0c19f01dad3f7e08e047800d0b2a5b102df20219
Jumsion05 <871888414@qq.com>
2018-10-18 19:14:34 +0800

    添加了主页面

--- login.py
+++ login.py
@@ -2,9 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.route("/login")
+@app.route("/login")
 def login():
     return "登陆界面"
 
+@app.route("/center")
+def center():
+    return "主页面"
+
+
 if __name__ == "__main__":
-    app.run()
+  app.run()
